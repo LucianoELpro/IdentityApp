71315b48e6be1c5755ebb2af3eb6295615ae36d2
LucianoElPro <lugel.vargas14@gmail.com>
2024-09-22 18:21:30 -0500

    fix regex email

--- Api/DTO/Account/ConfirmEmailDto.cs
+++ Api/DTO/Account/ConfirmEmailDto.cs
@@ -7,7 +7,7 @@ namespace Api.DTO.Account
         [Required]
         public string Token { get; set; }
         [Required]
-        [RegularExpression("^[\\w-\\.]+@[a-zA-Z_.]+?\\.[a-zA-Z]{2,3}$")]
+        [RegularExpression("^[\\w-\\.]+@[a-zA-Z_.]+?\\.[a-zA-Z]{2,3}$",ErrorMessage ="Invalid email address+")]
         public string Email { get; set; }
     }
 }
